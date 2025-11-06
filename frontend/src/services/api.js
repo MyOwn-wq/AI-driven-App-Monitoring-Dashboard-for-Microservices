b777c5968ab19fa35e0e78632c78df60e34fe937
Hemant Shinde <noobbotkey@gmail.com>
2025-11-06 13:40:33 +0530

    Fix the new error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
+console.log("Loaded API URL:", import.meta.env.VITE_API_URL);
+
+
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
